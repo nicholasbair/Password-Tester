f91b31a6da154e6dc9ec69bb0efef2a9e02eb1f7
nicholasbair <nicholasbair@Nicholass-MBP.home>
2015-09-20 10:51:36 -0400

    Fix syntax error
    Missing semi-colon line 13.

--- password.js
+++ password.js
@@ -10,7 +10,7 @@ var startTime = +new Date(),
     allChar = ['1','2','3','4','5','6','7','8','9','0','q','w','e','r','t','y',
 'u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m',
 'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z',
-'X','C','V','B','N','M']
+'X','C','V','B','N','M'];
 
 function writeToPage(id) {
     document.getElementById(id).innerHTML = message;
